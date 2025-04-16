43ecf112efc92708b7a3f3e94e20cd087d2b4778
RaviAchyar <ravistikombwi@gmail.com>
2025-04-16 07:44:39 +0700

    Add files via upload

---
+++ Tugas Pertemuan 4/StudyKasus_FOR-LOOP.py
@@ -0,0 +1,15 @@
+# M RAVI ACHYAR TRISTA ZHAID
+# 1124102193
+
+# Daftar harga barang
+harga_barang = [5000, 12000, 7500, 10000, 3000]
+
+# Inisialisasi total belanja
+total = 0
+
+# Gunakan for loop untuk menghitung total
+for harga in harga_barang:
+    total += harga
+
+# Tampilkan total belanja
+print("Total belanja adalah:", total)
---
+++ Tugas Pertemuan 4/StudyKasus_WHILE-LOOP.py
@@ -0,0 +1,11 @@
+jawaban = 7
+tebakan = 0
+
+while tebakan != jawaban:
+    tebakan = int(input("Tebak angka (1-10): "))
+    if tebakan < jawaban:
+        print("Terlalu kecil!")
+    elif tebakan > jawaban:
+        print("Terlalu besar!")
+    else:
+        print("Tebakan benar!")
